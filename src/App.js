c262a1c1f6358a41e26499ee1f7090f00fec2126
Devil7DK <dineshthangavel47@gmail.com>
2024-01-26 00:27:01 +0530

    Implement game winner detection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import "./App.scss";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { GameGrid } from "./components";
+
+const WinningCombinations = [
+  /**
+   * [X, X, X]
+   * [?, ?, ?]
+   * [?, ?, ?]
+   */
+  [0, 1, 2],
+  /**
+   * [?, ?, ?]
+   * [X, X, X]
+   * [?, ?, ?]
+   */
+  [3, 4, 5],
+  /**
+   * [?, ?, ?]
+   * [?, ?, ?]
+   * [X, X, X]
+   */
+  [6, 7, 8],
+  /**
+   * [X, ?, ?]
+   * [X, ?, ?]
+   * [X, ?, ?]
+   */
+  [0, 3, 6],
+  /**
+   * [?, X, ?]
+   * [?, X, ?]
+   * [?, X, ?]
+   */
+  [1, 4, 7],
+  /**
+   * [?, ?, X]
+   * [?, ?, X]
+   * [?, ?, X]
+   */
+  [2, 5, 8],
+  /**
+   * [X, ?, ?]
+   * [?, X, ?]
+   * [?, ?, X]
+   */
+  [0, 4, 8],
+  /**
+   * [?, ?, X]
+   * [?, X, ?]
+   * [X, ?, ?]
+   */
+  [2, 4, 6],
+];
 
 export const App = () => {
   // State to keep track of the current player
@@ -10,6 +61,27 @@ export const App = () => {
 
   // Set up the state for the values of 3x3 grid as an array of 9 with empty strings as default values
   const [values, setValues] = useState(() => new Array(9).fill(""));
+
+  useEffect(() => {
+    const winner = ["X", "O"].find((player) => {
+      return WinningCombinations.find((combination) => {
+        return combination.every((index) => values[index] === player);
+      });
+    });
+
+    if (winner) {
+      alert(`${winner} won!`);
+      setValues(new Array(9).fill(""));
+      return;
+    }
+
+    const isDraw = values.every((value) => value !== "");
+
+    if (isDraw) {
+      alert("Draw!");
+      setValues(new Array(9).fill(""));
+    }
+  }, [values]);
 
   return (
     <>

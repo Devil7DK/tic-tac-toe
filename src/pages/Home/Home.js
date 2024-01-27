d4f5041978e872ccb44e11145effde877dc23763
Devil7DK <dineshthangavel47@gmail.com>
2024-01-27 16:11:27 +0530

    Add title to describe the mode

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,16 +7,32 @@ export const Home = () => {
       <h1>Tic Tac Toe</h1>
 
       <div className="game-modes">
-        <Link to="/game/single" className="blue-button">
+        <Link
+          to="/game/single"
+          className="blue-button"
+          title="Play agains the computer"
+        >
           Single Player
         </Link>
-        <Link to="/game/offline" className="blue-button">
+        <Link
+          to="/game/offline"
+          className="blue-button"
+          title="Play against a friend on the same device"
+        >
           Offline Multiplayer
         </Link>
-        <Link to="/game/anonymous" className="blue-button">
+        <Link
+          to="/game/anonymous"
+          className="blue-button"
+          title="Play against a stranger online"
+        >
           Anonymous Multiplayer
         </Link>
-        <Link to="/game/online" className="blue-button">
+        <Link
+          to="/game/online"
+          className="blue-button"
+          title="Play against a friend online by sharing the room code."
+        >
           Online Multiplayer
         </Link>
       </div>
